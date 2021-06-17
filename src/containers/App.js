0bff0160d2b4cef3bba7caf92f0739e09c231843
John Florez <john.florez25@gmail.com>
2021-06-17 12:49:01 -0500

    Agregando estilos globales y convirtiendo responsive nuestro portafolio digital geek

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,11 +6,24 @@ import About from "../components/About.jsx";
 import Education from "../components/Education";
 import Experience from "../components/Experience";
 import Certificate from "../components/Certificate";
-import Skills from '../components/Skills'
+import Skills from '../components/Skills';
+
+import { createGlobalStyle  } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        font-family: 'Lato', sans-serif;
+        margin: 0;
+        padding: 0;
+        background: #F5F5F5;
+    }
+    
+`;
 
 const App = () => {
   return (
     <Main>
+      <GlobalStyle />
       <SideBar>
         <About />
       </SideBar>
